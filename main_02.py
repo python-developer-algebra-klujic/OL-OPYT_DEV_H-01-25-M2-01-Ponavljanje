5a2b222529b215bfd5f2f3a6ea18c5bf76bbef6b
python-developer-algebra-klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-03 20:56:45 +0100

    Summary

---
+++ main_02.py
@@ -0,0 +1,12 @@
+a = 5
+b = 3
+
+print()
+print(f'{a} + {b} = {a + b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
+print(f'{a} / {b} = {round((a / b), 2)}')
+print(f'{a} % {b} = {a % b}')
+print()
+
+# Funkcija round(broj, broj_decimala) zaokruzije broj na definirani broj decimala
